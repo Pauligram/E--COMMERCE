40c46fff60e2c39c1afada7bd3c4818c9ca5abc5
paulg <ololagbekeloluwa@yahoo.com>
2019-11-20 16:12:53 +0100

    updated

---
+++ main.js
@@ -0,0 +1,32 @@
+//variable declaration
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.product-center');
+
+var cart = [];
+
+//getting the products
+class products{
+
+}
+
+//display products
+class UI{
+
+}
+
+//local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ui = new UI();
+    const products = new products();
+});
